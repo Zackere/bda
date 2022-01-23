11004313bb37045e41f558060b025b331164afd5
Wojciech Replin <wreplin@opera.com>
2022-01-23 11:47:00 +0100

    fixup! Split aggregations by city and upload historical aggregations to mongodb

--- historical-data/serve.js
+++ historical-data/serve.js
@@ -10,7 +10,7 @@ const aggregates = {};
 
 ['delhi', 'berlin', 'warsaw', 'moscow'].forEach(city => {
   ['weather', 'pollution'].forEach(category => {
-    const buf = readFileSync(`${category}/${city}.json`);
+    const buf = readFileSync(`data/${category}/${city}.json`);
     const ret = JSON.parse(buf.toString());
     ret.sort((a, b) => a.measured - b.measured);
     ret.forEach((v, i) => {
@@ -22,17 +22,21 @@ const aggregates = {};
     all_data[category + city].forEach(v => (v.kafkatopic = category + city));
 
     aggregates[category + city] = [];
-    Object.entries(ret.map(v => {
-      v.date = new Date(v.measured * 1000);
-      v.date.setHours(0, 0, 0, 0);
-      return v;
-    }).reduce((dict,v) => {
-      (dict[v.date] = dict[v.date] || []).push(v);
-      return dict;
-    }, {})).forEach(([date,values]) => {
+    Object.entries(
+      ret
+        .map(v => {
+          v.date = new Date(v.measured * 1000);
+          v.date.setHours(0, 0, 0, 0);
+          return v;
+        })
+        .reduce((dict, v) => {
+          (dict[v.date] = dict[v.date] || []).push(v);
+          return dict;
+        }, {}),
+    ).forEach(([date, values]) => {
       const length = values.length;
       switch (category) {
-        case "weather":
+        case 'weather':
           const temps = values.map(x => x.temp);
           const pressures = values.map(x => x.pressure);
           const humidities = values.map(x => x.humidity);
@@ -40,20 +44,34 @@ const aggregates = {};
           const windspeeds = values.map(x => x.windspeed);
           const winddegs = values.map(x => x.winddeg);
           aggregates[category + city].push({
-            "mintemp": Math.min(...temps), "maxtemp": Math.max(...temps), "avgtemp": temps.reduce((a, b) => a + b, 0) / length,
-            "minpressure": Math.min(...pressures), "maxpressure": Math.max(...pressures), "avgpressure": pressures.reduce((a, b) => a + b, 0) / length,
-            "minhumidity": Math.min(...humidities), "maxhumidity": Math.max(...humidities), "avghumidity": humidities.reduce((a, b) => a + b, 0) / length,
-            "minclouds": Math.min(...clouds), "maxclouds": Math.max(...clouds), "avgclouds": clouds.reduce((a, b) => a + b, 0) / length,
-            "minwindspeed": Math.min(...windspeeds), "maxwindspeed": Math.max(...windspeeds), "avgwindspeed": windspeeds.reduce((a, b) => a + b, 0) / length,
-            "minwinddeg": Math.min(...winddegs), "maxwinddeg": Math.max(...winddegs), "avgwinddeg": winddegs.reduce((a, b) => a + b, 0) / length,
-            "date": +new Date(date)/1000
+            mintemp: Math.min(...temps),
+            maxtemp: Math.max(...temps),
+            avgtemp: temps.reduce((a, b) => a + b, 0) / length,
+            minpressure: Math.min(...pressures),
+            maxpressure: Math.max(...pressures),
+            avgpressure: pressures.reduce((a, b) => a + b, 0) / length,
+            minhumidity: Math.min(...humidities),
+            maxhumidity: Math.max(...humidities),
+            avghumidity: humidities.reduce((a, b) => a + b, 0) / length,
+            minclouds: Math.min(...clouds),
+            maxclouds: Math.max(...clouds),
+            avgclouds: clouds.reduce((a, b) => a + b, 0) / length,
+            minwindspeed: Math.min(...windspeeds),
+            maxwindspeed: Math.max(...windspeeds),
+            avgwindspeed: windspeeds.reduce((a, b) => a + b, 0) / length,
+            minwinddeg: Math.min(...winddegs),
+            maxwinddeg: Math.max(...winddegs),
+            avgwinddeg: winddegs.reduce((a, b) => a + b, 0) / length,
+            date: +new Date(date) / 1000,
           });
           break;
-        case "pollution":
+        case 'pollution':
           const aqis = values.map(x => x.aqi);
           aggregates[category + city].push({
-            "minaqi": Math.min(...aqis), "maxaqi": Math.max(...aqis), "avgaqi": aqis.reduce((a, b) => a + b, 0) / length,
-            "date": +new Date(date)/1000
+            minaqi: Math.min(...aqis),
+            maxaqi: Math.max(...aqis),
+            avgaqi: aqis.reduce((a, b) => a + b, 0) / length,
+            date: +new Date(date) / 1000,
           });
           break;
       }
@@ -63,11 +81,14 @@ const aggregates = {};
   });
 });
 
-const all_data_reduced_and_sorted = Object.values(all_data).reduce((p, c) => (p.push(...c), p), []);
+const all_data_reduced_and_sorted = Object.values(all_data).reduce(
+  (p, c) => (p.push(...c), p),
+  [],
+);
 all_data_reduced_and_sorted.sort((a, b) => a.measured - b.measured);
 for (const [key, values] of Object.entries(aggregates)) {
   values.sort((a, b) => a.date - b.date);
-  app.get('/' + key + 'aggregations', (_, res) => res.json(values));
+  app.get(`/${key}aggregations`, (_, res) => res.json(values));
 }
 app.get('/all', (req, res) => res.json(all_data_reduced_and_sorted));
 
